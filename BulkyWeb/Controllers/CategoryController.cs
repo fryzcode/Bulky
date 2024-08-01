a8879d49c23c87c5f1c226ba64ea1a3b40543983
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-08-01 12:08:47 +0400

    Create Controller for Category and added date to DB

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
