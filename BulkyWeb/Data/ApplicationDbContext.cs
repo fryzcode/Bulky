102785b03f4247e0e2623493bee79d1ffa534960
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-08-01 11:06:08 +0400

    Connect do DB

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }    
+}
