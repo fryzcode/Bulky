6dd199a6107fe93180de4bc5e6fd18608cfde693
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-08-02 16:06:33 +0400

    Created CategoryRepository

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -4,8 +4,9 @@ namespace Bulky.DataAccess.Repository.IRepository;
 
 public interface IRepository<T> where T : class
 {
-    IEnumerable<T> GetAll();
-    T Get(Expression<Func<T, bool>> filter);
+    //T - Category
+    IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter=null, string? includeProperties = null);
+    T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
     void Add(T entity);
     void Remove(T entity);
     void RemoveRange(IEnumerable<T> entity);
--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -1,46 +1,31 @@
-using System;
-using System.Collections.Generic;
 using System.Linq.Expressions;
 using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
+namespace Bulky.DataAccess.Repository;
+
 public class Repository<T> : IRepository<T> where T : class
 {
     private readonly ApplicationDbContext _db;
     internal DbSet<T> dbSet;
-    
+
     public Repository(ApplicationDbContext db)
     {
-        _db = db;
-        this.dbSet = _db.Set<T>();
-    }
-    
-    public void Add(T entity)
-    {
-        dbSet.Add(entity);
+        _db = db; this.dbSet = _db.Set<T>();
     }
 
-    public IEnumerable<T> GetAll()
+    public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
     {
-        IQueryable<T> query = dbSet;
-        return query.ToList();
+        IQueryable<T> query = dbSet; return query.ToList();
     }
 
-    public T Get(Expression<Func<T, bool>> filter)
+    public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
     {
-        IQueryable<T> query = dbSet;
-        query = query.Where(filter);
-        return query.FirstOrDefault();
+        IQueryable<T> query = dbSet; query = query.Where(filter); return query.FirstOrDefault();
     }
 
-    public void Remove(T entity)
-    {
-        dbSet.Remove(entity);
-    }
-
-    public void RemoveRange(IEnumerable<T> entity)
-    {
-        dbSet.RemoveRange(entity);
-    }
+    public void Add(T entity) { dbSet.Add(entity); } 
+    public void Remove(T entity) { dbSet.Remove(entity); } 
+    public void RemoveRange(IEnumerable<T> entity) { dbSet.RemoveRange(entity); }
 }
