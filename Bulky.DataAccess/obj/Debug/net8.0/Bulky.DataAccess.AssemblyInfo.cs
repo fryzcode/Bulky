8c79541b241b5429136ebfb078156130d2e07d43
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-08-13 11:00:07 +0400

    Add Helper Methods in OrderHeaderRepository

--- Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
+++ Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b8b6689a3bed131b05e983f4f0b8ad93663e25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16b229751036b1617d83206264208bb637872323")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Bulky.Utility/obj/Debug/net8.0/Bulky.Utility.AssemblyInfo.cs
+++ Bulky.Utility/obj/Debug/net8.0/Bulky.Utility.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Utility")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b8b6689a3bed131b05e983f4f0b8ad93663e25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16b229751036b1617d83206264208bb637872323")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Utility")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Utility")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
