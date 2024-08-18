6cc650f0493bcae643289b1a239f91a9e1a92ca2
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-08-18 11:55:07 +0400

    change bug

--- Bulky.Models/ViewModels/OrderVM.cs
+++ Bulky.Models/ViewModels/OrderVM.cs
@@ -2,6 +2,6 @@ namespace Bulky.Models.ViewModels;
 
 public class OrderVM
 {
-    public OrderHeader OrderHeader { get; set; }
-    public IEnumerable<OrderDetail> OrderDetail { get; set; }
+    public OrderHeader? OrderHeader { get; set; }
+    public IEnumerable<OrderDetail>? OrderDetail { get; set; }
 }
